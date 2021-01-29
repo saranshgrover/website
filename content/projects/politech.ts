c0a7adcd32e6fdb945ad76a3871e048f8f81a964
saranshgrover <contact@saranshgrover.com>
2021-01-29 09:02:55 -0500

    update politech

--- content/projects/politech.ts
+++ content/projects/politech.ts
@@ -3,8 +3,8 @@ import { research, ui, javascript, react} from './tags'
 
 const politech:  Project = {
     id: 'politech',
-    name: 'PoliTech Automated Redistricting',
-    description: 'A preimier Automated Redistricting System (ARS) for the rapid generation of statewide congressional districts through various user-defined preferences as well as fairness and correctness measures.',
+    name: 'PoliTech',
+    description: 'A premier Automated Redistricting System (ARS) for the rapid generation of statewide congressional districts through various user-defined preferences as well as fairness and correctness measures.',
     image: '/images/projects/politech.gif',
     isFeatured: true,
     tags: [research, ui, javascript, react],
