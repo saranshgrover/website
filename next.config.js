5b24d21878c2e61947acd4ade878b5c6774c00f1
Saransh Grover <contact@saranshgrover.com>
2021-06-07 00:20:46 -0400

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-    //https://github.com/vercel/next.js/issues/7755
-    webpack: (config, { isServer }) => {
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-            config.node = {
-                fs: 'empty'
-            }
-        }
-
-        return config
-    },
     target: 'serverless'
 }
