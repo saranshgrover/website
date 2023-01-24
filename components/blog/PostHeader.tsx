d56a17b7c41d06d3d5e4313a925fa93697d2993d
Saransh Grover <contact@saranshgrover.com>
2023-01-24 12:33:55 -0500

    update medium images

--- components/blog/PostHeader.tsx
+++ components/blog/PostHeader.tsx
@@ -16,13 +16,28 @@ interface Props {
 
 export default function PostHeader({ post, linkButton = false, minHeight = '50vh' }: Props): ReactElement {
 	const router = useRouter()
+	console.log(post.image)
 	const [gradient, description, gradientLevel] = useColorModeValue(
 		['#fff', 'gray.800', 'rgba(255,255, 255, 0.18)'],
 		['#000', 'gray.300', 'rgba(0, 0, 0, 0.18)']
 	)
 	return (
 		<Box pos='relative' minW='100vw' minH={minHeight}>
-			<Image src={post.image} layout='fill' objectFit='cover' />
+			{post.mdFile === 'medium' ? (
+				<span
+					style={{
+						width: '100vw',
+						boxSizing: 'border-box',
+						display: 'block',
+						overflow: 'hidden',
+						position: 'absolute',
+					}}
+				>
+					<img src={post.image} style={{ width: '100vw', objectFit: 'fill' }} />
+				</span>
+			) : (
+				<Image src={post.image} layout='fill' objectFit='cover' />
+			)}
 			<Container ml='auto' maxW='7xl' centerContent minH={minHeight}>
 				<Flex minH={minHeight} direction='column' justify='center' alignContent='center' alignItems='center'>
 					<Heading textAlign='center' h='100%' size='4xl' zIndex={2} letterSpacing={4} mb='2rem'>
