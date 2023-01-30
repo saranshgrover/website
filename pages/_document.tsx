93aa56bbcc91e5517726f4cfd5268bad6896e110
Saransh Grover <contact@saranshgrover.com>
2023-01-30 17:00:40 -0500

    update hotjar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,16 +7,19 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<Script id='hotjar'>
-						{`(function(h,o,t,j,a,r){
+					<script
+						id='hotjar'
+						dangerouslySetInnerHTML={{
+							__html: `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3344235,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-					</Script>
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+						}}
+					/>
 					{/* Global Site Tag (gtag.js) - Google Analytics */}
 					<script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TACKING_ID}`} />
 					<script
